3ade29055347818a34b9d1754c721dbf4ab6e3ae
alishah730 <alishah730@gmail.com>
2016-02-25 13:58:24 +0530

    commit

--- UserManagementService/src/main/java/com/techpedia/usermanagement/service/helper/UserManagementRestServiceHelper.java
+++ UserManagementService/src/main/java/com/techpedia/usermanagement/service/helper/UserManagementRestServiceHelper.java
@@ -77,6 +77,7 @@ import com.techpedia.usermanagement.service.response.UMServiceResponse;
 //import com.techpedia.usermanagement.dataobject.ProfileSearchCriteriaDO;
  //test git hub Working new changes
 //test git hub conflict here ali again commited on25th Feb....
+//added by charan
 @Service
 public class UserManagementRestServiceHelper {
 
